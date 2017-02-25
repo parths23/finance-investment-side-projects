8004044b5efc02b4ece452e869d9ca72b4e4c578
parths@syapse.com <parths@syapse.com>
2017-02-25 18:35:41 -0500

    split apart classes into separate files, added .gitignore, and added improvement ideas text script

---
+++ historical_analysis/portfolio.py
@@ -0,0 +1,96 @@
+"""Portfolio Class."""
+
+from yahoo_finance import Share
+
+
+class Portfolio():
+    """Portfolio class that represents a single run of a portfolio strategy over a certain time period."""
+
+    def __init__(self, rebalance_interval, start_year, end_year, monthly_contribution, stock_symbols):
+        self.cash_balance = 0.0
+        self.rebalance_interval = int(rebalance_interval)
+        self.start_year = int(start_year)
+        self.end_year = int(end_year)
+        self.initial_monthly_contribution = float(monthly_contribution)
+        self.current_month = 1
+        self.current_year = self.start_year
+        self.stocks = {}
+        self.stock_list = stock_symbols
+        self._initiate_stock_dict(stock_symbols)
+
+    def _initiate_stock_dict(self, stock_symbols):
+        stock_symbols = stock_symbols.strip().split(',')
+        for stock_symbol in stock_symbols:
+            self._update_stock_price(stock_symbol)
+
+    def _format_current_date(self):
+        return "{0}-{1}-25".format(self.current_year, self.current_month)
+
+    def _get_stock_price(self, stock_symbol):
+        try:
+            stock = Share(stock_symbol)
+            price = stock.get_historical(
+                self._format_current_date(), self._format_current_date())[0]['Open']
+            return float(price)
+        except:
+            return self.stocks[stock_symbol]['price']
+
+    def _update_stock_price(self, stock_symbol):
+        price = self._get_stock_price(stock_symbol)
+        if self.stocks.get(stock_symbol):
+            self.stocks[stock_symbol]['price'] = price
+            return price
+        else:
+            self.stocks[stock_symbol] = {'quantity': 0.0, 'price': price}
+            return price
+
+    def _purchase_stock(self, stock_symbol, cash_amount):
+        price = self._update_stock_price(stock_symbol)
+        stock_quantity = cash_amount / price
+        self.stocks[stock_symbol]['quantity'] = self.stocks[
+            stock_symbol]['quantity'] + stock_quantity
+        purchase_cost = stock_quantity * price
+        self.cash_balance -= purchase_cost
+
+    def _sell_stock(self, stock_symbol):
+        price = self.stocks[stock_symbol]['price']
+        quantity = self.stocks[stock_symbol]['quantity']
+        cash_back = price * quantity
+        self.cash_balance += cash_back
+        self.stocks[stock_symbol]['quantity'] = 0
+
+    def _buy_stocks(self):
+        stock_symbol_list = self.stocks.keys()
+        number_of_stocks = float(len(stock_symbol_list))
+        cash_per_stock = self.cash_balance / number_of_stocks
+        for stock_symbol in stock_symbol_list:
+            self._purchase_stock(stock_symbol, cash_per_stock)
+
+    def _transfer_cash(self):
+        multiplicative_factor = (self.current_year - self.start_year) * 250
+        amount_saved = self.initial_monthly_contribution + float(multiplicative_factor)
+        self.cash_balance += amount_saved
+
+    def _sell_stocks(self):
+        stock_symbol_list = self.stocks.keys()
+        for stock_symbol in stock_symbol_list:
+            self._sell_stock(stock_symbol)
+
+    def run(self, invest, rebalance):
+        """Method runs the portfolio from the January of start year to December of end year."""
+        years = range(self.start_year, self.end_year + 1)
+        for year in years:
+            self.current_year = year
+            for month in range(1, 13):
+                self.current_month = month
+                self._transfer_cash()
+                if invest:
+                    self._buy_stocks()
+                    if rebalance and self.current_month % self.rebalance_interval == 0:
+                        self._rebalance_portfolio()
+        self._sell_stocks()
+
+    def _rebalance_portfolio(self):
+        """Method rebalances the portfolio to equal distribution based on a parameter time interval."""
+        self._sell_stocks()
+        self._buy_stocks()
---
+++ historical_analysis/portfolio_analyzer.py
@@ -0,0 +1,40 @@
+"""Portfolio Manager."""
+from portfolio import Portfolio
+import csv
+
+
+class PortfolioAnalyzer():
+
+    def __init__(self, args):
+        self.portfolio_runs = []
+        self.args = args
+        self.rebalancing_intervals = [1, 2, 3, 4, 6, 12]
+
+    def _run_portfolio(self, args, invest, rebalance):
+        portfolio = Portfolio(
+            args.interval, args.start_year, args.end_year, args.monthly_contribution, args.stock_symbols)
+        portfolio.run(invest, rebalance)
+        self.portfolio_runs.append(portfolio)
+        print portfolio.cash_balance
+
+    def run_portfolio_models(self):
+        self.args.interval = 0
+        self._run_portfolio(self.args, invest=False, rebalance=False)
+        for interval in self.rebalancing_intervals:
+            self.args.interval = interval
+            self._run_portfolio(self.args, invest=True, rebalance=False)
+            self._run_portfolio(self.args, invest=True, rebalance=True)
+
+        self._sort_portfolio_runs()
+
+    def write_results_to_csv(self):
+        with open('results.csv', 'w') as csvfile:
+            fieldnames = ['start_year', 'end_year', 'stocks', 'rebalance_interval', 'end_balance']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for portfolio in self.portfolio_runs:
+                writer.writerow({'start_year': portfolio.start_year, 'end_year': portfolio.end_year, 'stocks': portfolio.stock_list,
+                                 'rebalance_interval': portfolio.rebalance_interval, 'end_balance': portfolio.cash_balance})
+
+    def _sort_portfolio_runs(self):
+        self.portfolio_runs.sort(key=lambda x: x.cash_balance, reverse=True)
